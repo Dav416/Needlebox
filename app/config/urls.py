98a7cf826782e0c91d0ac8ff95ffcbd158d8641b
Dav416 <david_zamu414@hotmail.com>
2021-07-19 20:15:05 +0100

    Agrego los ultimos cambios antes de modificar el proyecto, con la base de datos funcional que solo tiene la carpeta core, sin la carpeta erp

--- app/config/urls.py
+++ app/config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import myfirstview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('prueba/', myfirstview),
+
 ]
