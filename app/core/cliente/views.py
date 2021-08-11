c4d6f1fb6784778cc7da1ab679ea33b82eacba69
JuanPayares <juanpayares@misena.edu.co>
2021-08-11 12:32:29 -0500

    Forms y Datatable 2
    2

--- app/core/cliente/views.py
+++ app/core/cliente/views.py
@@ -1,14 +1,25 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from core.cliente.models import InfoClient, InfoGeneClient
+from core.cliente.forms import InfoClientForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 
 # Create your views here.
 class NeedleCliente(TemplateView):
     template_name = '../templates/clientes.html'
 
-class NeedleNuevoCliente(TemplateView):
-    template_name = '../templates/clientesnuevousu.html'
+class NeedleNuevoCliente(CreateView):
+    model = InfoClient
+    form_class = InfoClientForm
+    template_name = 'clientesnuevousu.html'
+    success_url = reverse_lazy('cliente3')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_list1'] = InfoClient.objects.all()
+        context['form1'] = InfoClientForm
+        return context
 
 class NeedleNuevoCliente2(TemplateView):
     template_name = '../templates/clientesnuevousu2.html'
@@ -19,5 +30,9 @@ class NeedleEditCliente(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['object_list1'] = InfoClient.objects.all()
-
+        context['form1'] = InfoClientForm
         return context
+
+
+
+
