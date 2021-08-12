d6808f3d1352a559ebf2d5bbf70f3f840be926ff
JF-MS <86071458+JF-MS@users.noreply.github.com>
2021-08-11 20:03:09 -0500

    Actualización gitignore y más

--- app/core/insumos/urls.py
+++ app/core/insumos/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.insumos.views import NeedleInsu, RegInsumoCreateView, RegProveedorCreateView
+from core.insumos.views import NeedleInsu, RegInsumoCreateView, RegProveedorCreateView, EditInsumoUpdateView
 
 # Este es el url de index
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('insumos/', NeedleInsu.as_view(), name="insumos"),
     path('insu/', RegInsumoCreateView.as_view(), name="registrar_insumo"),
     path('prov/', RegProveedorCreateView.as_view(), name="registrar_proveedor"),
+    path('editnsu/<int:pk>/', EditInsumoUpdateView.as_view(), name="editar_insumo"),
 ]
