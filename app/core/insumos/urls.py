8a4780471c934c02431c169279c779cfe158aa3a
JF-MS <86071458+JF-MS@users.noreply.github.com>
2021-08-13 03:30:24 -0500

    Trabajo en modulo insumos
    Se cambio extensamente el diseño de insumos, con el fin de poder crear las vistas de forma similar a las de los tutoriales. Se ha implementado igual las 4 vistas  y todos los métodos exigidos, pero falta verificar su ensayo y realizar correciones.

--- app/core/insumos/urls.py
+++ app/core/insumos/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from core.insumos.views import NeedleInsu, RegInsumoCreateView, RegProveedorCreateView, EditInsumoUpdateView
+from core.insumos.views import NeedleInsu, RegInsumoCreateView, RegProveedorCreateView, EditInsumoUpdateView, \
+    EditProveedorUpdateView, ListProveedoresView, ListInsumosView
 
 # Este es el url de index
 
 
 urlpatterns = [
-    path('insumos/', NeedleInsu.as_view(), name="insumos"),
-    path('insu/', RegInsumoCreateView.as_view(), name="registrar_insumo"),
-    path('prov/', RegProveedorCreateView.as_view(), name="registrar_proveedor"),
-    path('editnsu/<int:pk>/', EditInsumoUpdateView.as_view(), name="editar_insumo"),
+    path('opciones/', NeedleInsu.as_view(), name="insumos"),
+
+    path('lista_proveedores/', ListProveedoresView.as_view(), name="lista_proveedores"),
+    path('lista_insumos/', ListInsumosView.as_view(), name="lista_insumos"),
+
+    path('nuevo_insumo/', RegInsumoCreateView.as_view(), name="registrar_insumo"),
+    path('nuevo_proveedor/', RegProveedorCreateView.as_view(), name="registrar_proveedor"),
+
+    path('editar_insumo/<int:pk>/', EditInsumoUpdateView.as_view(), name="editar_insumo"),
+    path('editar_proveedor/<int:pk>/', EditProveedorUpdateView.as_view(), name="editar_proveedor"),
 ]
