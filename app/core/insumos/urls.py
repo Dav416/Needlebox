c1cd54af63132784b1a1ac7a544bf49662719b7d
JF-MS <86071458+JF-MS@users.noreply.github.com>
2021-08-15 19:49:28 -0500

    Insumos con create, update y delete funcionales
    Se crearon nuevas plantillas, para menús de confirmación de borrado de objetos/registros,  se crearon las views para cada tabla/formulario y se modificaron los igualmente a fin de lograr el funcionamiento de las tres vistas.

--- app/core/insumos/urls.py
+++ app/core/insumos/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from core.insumos.views import NeedleInsu, RegInsumoCreateView, RegProveedorCreateView, EditInsumoUpdateView, \
-    EditProveedorUpdateView, ListProveedoresView, ListInsumosView
+    EditProveedorUpdateView, ListProveedoresView, ListInsumosView, BorrarInsumoDeleteView, BorrarProveedorDeleteView
 
 # Este es el url de insumos
 
@@ -16,4 +16,7 @@ urlpatterns = [
 
     path('editar_insumo/<int:pk>/', EditInsumoUpdateView.as_view(), name="editar_insumo"),
     path('editar_proveedor/<int:pk>/', EditProveedorUpdateView.as_view(), name="editar_proveedor"),
+
+    path('delete_insumo/<int:pk>/', BorrarInsumoDeleteView.as_view(), name="borrar_insumo"),
+    path('delete_proveedor/<int:pk>/', BorrarProveedorDeleteView.as_view(), name="borrar_proveedor"),
 ]
