d08b55dcc2c10ebf6fb2ce96ac3d9c173ea7bc62
JuanPayares <juanpayares@misena.edu.co>
2021-08-18 13:27:15 -0500

    Cambios de insumos
    CRUD Pipe

--- app/core/insumos/forms.py
+++ app/core/insumos/forms.py
@@ -51,8 +51,21 @@ class RegInsumoForm(ModelForm):
                     'id': 'insu-unid',
                     'placeholder': 'Cm, kg, unidad, etc.'
                 }
-            )
+            ),
         }
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                form.save()
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error en forms'] = str(e)
+        return data
+
 # -----------------REGISTRO PROVEEEDORES------------------------------------
 
 
@@ -100,7 +113,20 @@ class RegProveedorForm(ModelForm):
                 attrs={
                     'id': 'ProvDir',
                     'placeholder': 'Ubicación del proveedor',
+                    'required': 'false',
                 }
             ),
 
         }
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                form.save()
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error en forms'] = str(e)
+        return data
