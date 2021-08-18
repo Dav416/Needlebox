2439032a739751f565eae2a22a73539ffd081636
JF-MS <86071458+JF-MS@users.noreply.github.com>
2021-08-18 13:55:12 -0500

    Cambios tras organizar gitignore de master
    Se correigieron errores menores en el html de login, se implemento el  nuevo diseño y crud de insumos, organizaron las views y modelos de clientes, las urls y settings de config y  se hicerieron nuevas migraciones, lo que implicó cambios en la bd

--- app/core/insumos/urls.py
+++ app/core/insumos/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
-from core.insumos.views import NeedleInsu, RegInsumoCreateView, RegProveedorCreateView, EditInsumoUpdateView
+from core.insumos.views import NeedleInsu, RegInsumoCreateView, RegProveedorCreateView, EditInsumoUpdateView, \
+    EditProveedorUpdateView, ListProveedoresView, ListInsumosView, BorrarInsumoDeleteView, BorrarProveedorDeleteView
 
-# Este es el url de index
+# Este es el url de insumos
 
 
 urlpatterns = [
-    path('insumos/', NeedleInsu.as_view(), name="insumos"),
-    path('insu/', RegInsumoCreateView.as_view(), name="registrar_insumo"),
-    path('prov/', RegProveedorCreateView.as_view(), name="registrar_proveedor"),
-    path('editnsu/<int:pk>/', EditInsumoUpdateView.as_view(), name="editar_insumo"),
+    path('opciones/', NeedleInsu.as_view(), name="insumos"),
+
+    path('lista_proveedores/', ListProveedoresView.as_view(), name="lista_proveedores"),
+    path('lista_insumos/', ListInsumosView.as_view(), name="lista_insumos"),
+
+    path('nuevo_insumo/', RegInsumoCreateView.as_view(), name="registrar_insumo"),
+    path('nuevo_proveedor/', RegProveedorCreateView.as_view(), name="registrar_proveedor"),
+
+    path('editar_insumo/<int:pk>/', EditInsumoUpdateView.as_view(), name="editar_insumo"),
+    path('editar_proveedor/<int:pk>/', EditProveedorUpdateView.as_view(), name="editar_proveedor"),
+
+    path('delete_insumo/<int:pk>/', BorrarInsumoDeleteView.as_view(), name="borrar_insumo"),
+    path('delete_proveedor/<int:pk>/', BorrarProveedorDeleteView.as_view(), name="borrar_proveedor"),
 ]
