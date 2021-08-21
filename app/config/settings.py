4e49692d65492a1601083da51deab0adbaf7725a
JuanPayares <juanpayares@misena.edu.co>
2021-08-21 13:22:28 -0500

    Cambios menores
    ...

--- app/config/settings.py
+++ app/config/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = [
 ]
 
 
-LOGIN_REDIRECT_URL = '/core/index/templates/index.html'
-
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- app/core/login/views.py
+++ app/core/login/views.py
@@ -1,20 +1,24 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
+from core.login.forms import RegistrarUsuarioForm
 from django.shortcuts import render
 
 
 # Create your views here.
-class NeedleLogin(TemplateView):
+class NeedleLogin(LoginView):
     template_name = '../templates/loginprincipal.html'
-
-
-class NeedleRegis(LoginView):
-    template_name = '../templates/loginregusu.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Regístrese por primera vez'
+        context['title'] = 'Inicie sesión'
+        context['form1'] = RegistrarUsuarioForm
         return context
+
+
+class NeedleRegis(TemplateView):
+    template_name = '../templates/loginregusu.html'
+
+
 
 class NeedleRecpass(TemplateView):
     template_name = '../templates/loginrecpass.html'
